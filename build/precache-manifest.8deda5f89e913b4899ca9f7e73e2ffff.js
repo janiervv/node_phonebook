f5fb2a09c038e4dee7fa7d0bf5044a2133fdfc3d
Jani Ervasti <jani.ervasti@mail.com>
2020-10-10 20:29:24 +0300

    build lisätty

---
+++ build/precache-manifest.8deda5f89e913b4899ca9f7e73e2ffff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f60a05de0f8d126350eb7d60c027228f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9f7263d80020038c6aa",
+    "url": "/static/css/main.e276c018.chunk.css"
+  },
+  {
+    "revision": "a6a8cca3e8d33493b566",
+    "url": "/static/js/2.dd6900d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dd6900d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9f7263d80020038c6aa",
+    "url": "/static/js/main.cda4f00c.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
